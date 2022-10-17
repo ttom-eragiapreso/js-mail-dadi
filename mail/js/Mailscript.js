9f0550977aa6d2da7ab1e5f716020ac3a5ad0874
ttom-eragiapreso <109075301+ttom-eragiapreso@users.noreply.github.com>
2022-10-17 10:02:54 +0200

    Incluso anche l'opzione senza includes

--- mail/js/Mailscript.js
+++ mail/js/Mailscript.js
@@ -5,8 +5,27 @@ let userEmail = prompt("Inserisci la tua email");
 emailList = ["nome.cognome@gmail.com", "enrico.michelotto@gmail.com", "tizio.caio@gmail.com"]
 
 // Se l'array di email registrate contiene l'email inserita dall'utente, allora scrivo bentornato, altrimenti un alert dice che non sei in lista.
+// Modalità più moderna e veloce
 if(emailList.includes(userEmail)){
   console.log("Bentornato Utente")
 }else {
   alert("La tua email non è in lista")
+}
+
+
+
+
+// Modalità "a mano" con il ciclo e variabile flag.
+let isInList = false;
+
+for(let i = 0; i < emailList.length; i++){
+  if(userEmail === emailList[i]){
+    isInList = true
+  }
+}
+
+if(isInList){
+  console.log("Sei in lista")
+}else {
+  console.log("Non sei in lista")
 }
